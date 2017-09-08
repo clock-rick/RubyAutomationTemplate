72fd253000fa3208b4fd23ed48a1c332e4ab8b1c
rickhodgett <rick.hodgett@gmail.com>
2017-09-08 17:00:51 -0500

    Initial commit

---
+++ Pages/abstract_base_page.rb
@@ -0,0 +1,9 @@
+class AbstractBasePage
+  def initialize(browser, url_extension)
+    @base_url = 'Enter root URL here'
+    @url_extension = url_extension
+    @browser = browser
+    @driver = Selenium::WebDriver::Driver.for @browser
+    @driver.navigate.to @base_url + @url_extension
+  end
+end
---
+++ Pages/rename_this_page.rb
@@ -0,0 +1,25 @@
+# This area will contain required gems and pages.
+require 'selenium-webdriver'
+require_relative 'abstract_base_page'
+
+# Enter in the url for the child page here.
+@url_extension = 'Child Page url eg /contacts'
+
+# This class will contain all the page definitions you want to create. The Class inherits from the AbstractBasePage
+class RenameThisPage < AbstractBasePage
+
+  # Below is an example of a definition. This will contain the logic for whatever feature you create.
+  def some_functionality
+
+    # Declare any variables below.
+    test_string = 'This is a string variable.'
+    num1 = 2
+    bool_1 = true
+
+    # Capture WebElements here
+    name_field = @driver.find_element(:id, 'form_223_0')
+
+    # Enter logic below.
+    name_field.action.send_keys('Joe Cool')
+  end
+end
